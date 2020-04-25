3209acf69fc305a112a564cebdd4904ca2238baa
Abhinav Dwivedi <abhinavdwivedi440@gmail.com>
2020-04-25 20:21:37 +0530

    built the command line part

---
+++ cmd/do.go
@@ -0,0 +1,29 @@
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"strconv"
+)
+
+var doCmd = &cobra.Command{
+	Use:   "do",
+	Short: "Marks a task as complete",
+	Run: func(cmd *cobra.Command, args []string) {
+		var ids []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to pass the argument:", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+		fmt.Println(ids)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(doCmd)
+}
