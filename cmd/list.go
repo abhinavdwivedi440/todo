ff08fa27d2fc695a21a44a872460bb4d71eb775f
Abhinav Dwivedi <abhinavdwivedi440@gmail.com>
2020-04-26 13:48:02 +0530

    updated list.go for the  list command  to print the list of tasks

--- cmd/list.go
+++ cmd/list.go
@@ -2,15 +2,28 @@ package cmd
 
 import (
 	"fmt"
-
+	"github.com/abhinavdwivedi440/todo/db"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists all of your tasks.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("You have no tasks to complete! Why not take a vacation ?")
+			return
+		}
+		fmt.Println("You have the following tasks:")
+		for i, task := range tasks {
+			fmt.Println("%d. %s\n", i+1, task.Value)
+		}
 	},
 }
 
