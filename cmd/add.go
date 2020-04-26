05c982347de29f9914ae4bd4c6f9b512c5ceafc6
Abhinav Dwivedi <abhinavdwivedi440@gmail.com>
2020-04-26 15:41:49 +0530

    updated do.go for do command to mark the task as complete

--- cmd/add.go
+++ cmd/add.go
@@ -10,11 +10,12 @@ import (
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Adds a task to your task list.",
-	Long: "\"add\" command  adds a task to your task list.\n\nUsage:\n  todo add \"Your task\"",
+	Long: "\"add\" command  adds a task to your task list.\n\nUsage:\n  todo add \"Your task\"\nHere quotes are optional\n  todo add Your task\n",
 	Run: func (cmd *cobra.Command, args []string) {
 			task := strings.Join(args, " ")
 			_, err := db.CreateTask(task)
 			if err != nil {
+				fmt.Println("Something went wrong:", err)
 				return
 			}
 			fmt.Printf("Added \"%s\" to your task list.\n", task)
