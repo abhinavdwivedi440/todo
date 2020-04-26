6000fbe405498efb5294a74a450d1e0d652b8556
Abhinav Dwivedi <abhinavdwivedi440@gmail.com>
2020-04-26 13:08:48 +0530

    created a tasks bucket

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/abhinavdwivedi440/todo/cmd"
-
+import (
+	"fmt"
+	"github.com/abhinavdwivedi440/todo/cmd"
+	"github.com/abhinavdwivedi440/todo/db"
+	"github.com/mitchellh/go-homedir"
+	"os"
+	"path/filepath"
+)
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, ".tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
 
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
